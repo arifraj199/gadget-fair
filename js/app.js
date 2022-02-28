32459075834d7c8134c12b48cb236c215ce27f02
arifraj199 <karif9514@gmail.com>
2022-03-01 00:31:46 +0600

    search result showing

--- js/app.js
+++ js/app.js
@@ -10,6 +10,27 @@ const loadPhone = searchText => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(value => displayPhone(value.data));
 }
-// loadPhone();
+
+const displayPhone = phones => {
+    // console.log(phones);
+    const displayPhone = document.getElementById('display-phone');
+    const error = document.getElementById('error');
+    phones.forEach(phone => {
+        // console.log(phone);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="h-100">
+            <img src="${phone.image}" alt="...">
+            <div class="card-body">
+                <h4>Phone: ${phone.phone_name}</h4>
+                <h5>Brand: ${phone.brand}</h5>
+                <button class="explore-button">Explore</button>
+            </div>
+        </div>
+        `;
+        displayPhone.appendChild(div);
+    })
+}
